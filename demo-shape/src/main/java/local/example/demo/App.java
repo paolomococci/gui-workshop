f17435ed2e9afd53ad3a5589dd216796ed4b7cb1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-13 21:45:56 +0100

    Create App.java

---
+++ demo-shape/src/main/java/local/example/demo/App.java
@@ -0,0 +1,38 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.demo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * demo shape in 3d App
+ */
+public class App 
+        extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

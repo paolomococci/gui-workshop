9fe6c688303e773c44c40c13c4b800030379da23
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-15 17:59:30 +0100

    Create Face.java

---
+++ demo-chart/src/main/java/local/example/demo/element/Face.java
@@ -0,0 +1,65 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.demo.element;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
+public class Face 
+        extends Pane {
+
+    Rectangle rectangleSide;
+
+    public Face(double size) {
+        rectangleSide = new Rectangle(size, size);
+        getChildren().add(rectangleSide);
+        var translatioOfTheZAxis = 0.0;
+        var widthOfLine = 1.0;
+        Color colorOfGrid = Color.GRAY;
+        for (int translationOfTheYAxis = 0;
+                translationOfTheYAxis <= size;
+                translationOfTheYAxis += size / 100) {
+            Line line = new Line(0, 0, size, 0);
+            line.setStroke(colorOfGrid);
+            line.setFill(colorOfGrid);
+            line.setTranslateY(translationOfTheYAxis);
+            line.setTranslateZ(translatioOfTheZAxis);
+            line.setStrokeWidth(widthOfLine);
+            getChildren().addAll(line);
+        }
+        for (int translationOfTheXAxis = 0; 
+                translationOfTheXAxis <= size; 
+                translationOfTheXAxis += size / 100) {
+            Line line = new Line(0, 0, 0, size);
+            line.setStroke(colorOfGrid);
+            line.setFill(colorOfGrid);
+            line.setTranslateX(translationOfTheXAxis);
+            line.setTranslateZ(translatioOfTheZAxis);
+            line.setStrokeWidth(widthOfLine);
+            getChildren().addAll(line);
+        }
+    }
+
+    public void setFill(Paint paint) {
+        rectangleSide.setFill(paint);
+    }
+}
